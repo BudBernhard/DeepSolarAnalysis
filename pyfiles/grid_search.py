f25f7c26c19bf67bf2f883bf60a577b63f724996
Allison Lee <allison.alee@gmail.com>
2019-12-16 16:17:14 -0500

    Wrote code for gridsearching the best model

--- pyfiles/grid_search.py
+++ pyfiles/grid_search.py
@@ -8,39 +8,32 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn..svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
-
-# Decision Tree Classifier
-def tune_decision_tree_hyperparameters(X_train, y_train):
-    """This function performs a grid search on the hyperparamters for a decision tree classifier, and returns the optimal parameters."""
-    
-    parameters = {'max_depth':[2, 3, 4, 5, 8], 'min_samples_split':[2, 5, 10, 15, 100], 'min_samples_leaf':[1, 2, 5, 10]}
-    dt = GridSearchCV(DecisionTreeClassifier(), parameters, cv = 5)
-    dt.fit(X_train, y_train)
-    return dt.best_params_
+from sklearn.model_selection import GridSearchCV
 
 
-def tune_random_forest_hyperparameters(X_train, y_train):
-    """This function performs a grid search on the hyperparamters for a random forests classifier and returns the optimal parameters."""
-    
-    params ={'n_estimators': [120, 300, 500, 800], 'max_depth':[5, 8, 15, 25, 30], 'min_samples_split':[1, 2, 5, 10, 15, 100], 'min_samples_leaf'=[1, 2, 5, 10], 'max_features'= ['log2', 'sqrt', None]
-    
-    rf = GridSearchCV(RandomForestClassifier(), params, cv=5)
-    rf.fit(X_train, y_train)
-    
-    return rf.best_params_
-             
-             
-def tune_svm_hyperparameters(X_train, y_train):
-    """This function performs a grid search on the hyperparameters for an SVM model and returns the optimal parameters."""
-    params = {'C':[.001, .01, 0.1, 1, 10, 100, 1000], 'gamma': ['auto','scale'], 'class_weight' : ['balanced', None]}
-             
-    svm = GridSearchCV(SVC(), params, cv=5)
-    svm.fit(X_train, y_train)
-    return svm.best_params_
-             
-             
-def tune_knn_hyperparameters(X_train, y_train):
-     """This function performs a grid search on the hyperparameters for a KNN model and returns the optimal parameters."""
-    params = {'n_neighbors': [2, 4, 8, 16], 'p': [2,3]}
-    knn = GridSearchCV(KNeighborsClassifier(), params, cv = 5)
-    return knn.best_params_
+
+pipe = Pipeline([('classifier', DecisionTreeClassifier())])
+
+search_space = [{'classifier': [DecisionTreeClassifier()],
+                 'classifier__max_depth': [2, 3, 4, 5, 8],
+                 'classifier__min_samples_split': [2, 5, 10, 15, 100]
+                'classifier__min_samples_leaf':[1, 2, 5, 10]},
+                {'classifier': [RandomForestClassifier()],
+                 'classifier__n_estimators': [10, 100, 1000],
+                 'classifier__max_depth': [5, 8, 15, 25, 30], 
+                 'classifier__min_samples_split':[1, 2, 5, 10, 15, 100],
+                 'classifier__min_samples_leaf': [1, 2, 5, 10],
+                 'classifier__max_features': ['log2', 'sqrt', None]}
+               {'classifier': [SVC()],
+               'classifier__C': [.001, .01, 0.1, 1, 10, 100, 1000],
+                   'classifier__gamma': ['auto','scale'],
+                   'classifier__class_weight':['balanced', None]}
+               {'classifier':[KNeighborsClassifier()],
+                   'classifier__n_neighbors':[2,4,8,16],
+                   'classifier__p':[2,3]}]
+
+clf = GridSearchCV(pipe, search_space, cv=5, verbose=0)
+
+best_model = clf.fit(X_train, y_train)
+
+best_model.best_estimator_.get_params()['classifier']
